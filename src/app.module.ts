8cb4431d6704e6e7b288c7368b04e41ab53d379e
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-10-24 00:48:59 -0300

    Configured .env. Integrated with keycloak

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KeycloakModule } from './keycloak.module';
 
 @Module({
-  imports: [],
+  imports: [KeycloakModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/keycloak.module.ts
@@ -0,0 +1,51 @@
+import { Module } from '@nestjs/common';
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  AuthGuard,
+} from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
+
+// Based on: https://medium.com/devops-dudes/secure-nestjs-rest-api-with-keycloak-745ef32a2370
+
+@Module({
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: process.env.KEYCLOAK_HOST,
+      realm: process.env.KEYCLOAK_REALM,
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      secret: process.env.KEYCLOAK_CLIENT_SECRET,
+    }),
+  ],
+  providers: [
+    // This adds a global level authentication guard,
+    // you can also have it scoped
+    // if you like.
+    //
+    // Will return a 401 unauthorized when it is unable to
+    // verify the JWT token or Bearer header is missing.
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and 
+    // methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    // New in 1.1.0
+    // This adds a global level role guard, which is permissive.
+    // Used by `@Roles` decorator with the 
+    // optional `@AllowAnyRole` decorator for allowing any
+    // specified role passed.
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
+})
+export class KeycloakModule {}
