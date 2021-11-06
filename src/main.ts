a368a45c2c023a7bf5f097c78cf7c0cbfc3ecef8
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-11-06 15:42:27 -0300

    Enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: '*' })
   app.useGlobalPipes(new ValidationPipe())
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
