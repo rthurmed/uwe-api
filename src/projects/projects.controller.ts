084ae112fba4d4e1b7dcbd7bced2c6b0ec41520c
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-12-15 11:32:36 -0300

    Implemented search option on project endpoint

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,17 +1,22 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
+  Query,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthenticatedUser, Resource } from 'nest-keycloak-connect';
+import { Pagination } from 'nestjs-typeorm-paginate';
 import { PermissionsService } from 'src/permissions/permissions.service';
-import { FindConditions, FindManyOptions, In } from 'typeorm';
+import { FindConditions, In, Like } from 'typeorm';
 import { BaseController } from '../core/base.controller';
 import { CreateProjectDTO } from './dto/create-project.dto';
 import { UpdateProjectDTO } from './dto/update-project.dto';
@@ -30,18 +35,38 @@ export class ProjectController extends BaseController<Project> {
     super(projectService, ['permissions', 'diagrams']);
   }
 
-  override async getSearchOptions(
-    request: Request,
-    user: any,
-  ): Promise<FindConditions<Project> | FindManyOptions<Project>> {
+  @Get()
+  async search(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(20), ParseIntPipe) limit = 20,
+    @Query('name') name,
+    @Req() request: Request,
+    @AuthenticatedUser() user,
+  ): Promise<Pagination<Project>> {
     // Project are only shown if the user have the right permissions
-    const ids = (
+    const projectIds = (
       await this.permissionService.findAcceptedByUserId(user.sub)
     ).map((e) => e.projectId);
-    return {
-      where: { id: In(ids) },
-      relations: ['permissions', 'diagrams'],
+
+    const findConditions: FindConditions<Project> = {
+      id: In(projectIds),
     };
+
+    if (name) {
+      findConditions.name = Like(`%${name}%`);
+    }
+
+    return this.projectService.paginate(
+      {
+        page,
+        limit,
+        route: request.url,
+      },
+      {
+        where: findConditions,
+        relations: ['permissions', 'diagrams'],
+      },
+    );
   }
 
   @Post()
